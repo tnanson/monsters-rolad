ef804ffb94861a1c3f93e0adc33794eda2dff2bd
unknown <nansont@yahoo.com>
2020-08-22 11:14:01 -0400

    Updates

---
+++ precache-manifest.fb41a2a235646ad51962626ccfd658d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17d1c1ae2906d435a8789b2c42a1b1a0",
+    "url": "/monsters-rolad/index.html"
+  },
+  {
+    "revision": "b5f848f79bfb0c3f34af",
+    "url": "/monsters-rolad/static/css/main.c6ffbb40.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolad/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolad/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5f848f79bfb0c3f34af",
+    "url": "/monsters-rolad/static/js/main.d74a635e.chunk.js"
+  },
+  {
+    "revision": "1d63417d9821b6498d15",
+    "url": "/monsters-rolad/static/js/runtime-main.b61c32cb.js"
+  }
+]);
